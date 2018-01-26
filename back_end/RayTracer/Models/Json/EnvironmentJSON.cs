c416b51966246b4853895e2b81d390b7998cef47
Nagarjuna <nag6452@yahoo.co.in>
2018-01-26 15:50:03 +0000

    Adding GeometryJSON and EnvironmentJSON

---
+++ back_end/RayTracer/Models/Json/EnvironmentJSON.cs
@@ -0,0 +1,29 @@
+﻿using rayTracer.Models.Elements;
+using RayTracer.Models.Cameras;
+using RayTracer.Models.Lights;
+using RayTracer.Models.SceneElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RayTracer.Models.Json
+{
+    public class EnvironmentJSON
+    {
+        string fileName;
+        WindowFrame winFrame;
+        ColorRGB background;
+        Light light;
+        Camera camera;
+
+        public EnvironmentJSON(string fileName, WindowFrame winFrame, ColorRGB background, Light light, Camera camera)
+        {
+            this.fileName = fileName;
+            this.winFrame = winFrame;
+            this.background = background;
+            this.light = light;
+            this.camera = camera;
+        }
+    }
+}

c3e2ad74a41a270851be2b32456e9e3474ea23bc
Timothy <obimmatimothy@yahoo.com>
2018-03-02 13:57:36 +0000

    2 test cases done

--- src/RayTracer.Tests/Controllers/SphereTest.cs
+++ src/RayTracer.Tests/Controllers/SphereTest.cs
@@ -9,12 +9,28 @@ namespace RayTracer.Tests.Controllers
     [TestFixture]
     public class SphereTest
     {
+        
         [Test]
         public void TestIntersection_DiscriminantLessThan0()
         {
             Sphere testSphere = new Sphere();
             HitInfo hitInfo = testSphere.Intersect(new Ray(new Point3D(0), new Vector3D(0)));
             Assert.IsFalse(hitInfo.hasHit);
+        }
+
+        [Test]
+        public void TestIntersection_DenominatorEquals0()
+        {
+            Sphere testSphere = new Sphere();
+            HitInfo hitInfo = testSphere.Intersect((new Ray(new Point3D(0), new Vector3D(0))));
+            Assert.IsFalse(hitInfo.hasHit);
+        }
+
+        [Test]
+        public void TestTGreaterthanKEpsilon()
+        {
+            Sphere testSphere = new Sphere();
+
         }
     }
 }

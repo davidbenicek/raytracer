9e4a3af74fde3c8961ef969a088a0b4a53e004ef
Othman <othmankhamra@gmail.com>
2018-03-02 18:35:41 +0000

    * Test.cs: * RayTracer.Tests.csproj:
    * FlatTest.cs: Add Test cases for flat material
    
    * RegularSamplingTest.cs: Add some test cases to check if the
      generation of samples is right
    
    * Point2D.cs: Implement Equals method.
    
    * Sampler.cs: Add GetSampling Method to get the sampling list

---
+++ src/RayTracer.Tests/MaterialsTesting/FlatTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using RayTracer.Models.Elements;
+using RayTracer.Models.Materials;
+using RayTracer.Models.Util;
+
+namespace RayTracer.Tests.MaterialsTesting
+{
+    [TestFixture]
+    public class FlatTest
+    {
+        [Test]
+        public void TestCalculateShade_DefaultColor()
+        {
+            //Arrange
+            Flat testFlat = new Flat();
+            //Act
+            ColorRGB actualColor = testFlat.CalculateShade(null);
+            //Assert
+            Assert.IsTrue(actualColor.Equals(Config.WHITE));
+        }
+
+        [Test]
+        public void TestCalculateShade_CustomColor()
+        {
+            //Arrange
+            Flat testFlat = new Flat(new ColorRGB(0.5));
+            //Act
+            ColorRGB expectedColor = new ColorRGB(0.5);
+            ColorRGB actualColor = testFlat.CalculateShade(null);
+            //Assert
+            Assert.IsTrue(actualColor.Equals(expectedColor));
+        }
+    }
+}

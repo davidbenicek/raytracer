7fcd5942decc4c630f722e0a2b41d3000493e842
Timothy <obimmatimothy@yahoo.com>
2018-03-03 15:15:27 +0000

    changes to CubeTest

--- src/RayTracer.Tests/GeometricTesting/CubeTest.cs
+++ src/RayTracer.Tests/GeometricTesting/CubeTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using RayTracer.Models.Elements;
 using RayTracer.Models.Geometric;
 using RayTracer.Models.Materials;
+using RayTracer.Models.Util;
 
 namespace RayTracer.Tests.GeometricTesting
 {
@@ -26,9 +27,30 @@ namespace RayTracer.Tests.GeometricTesting
 
         }
 
-        public void TestSum()
+        [Test]
+        public void TestRayDirectionEqualszero()
         {
-            
+            Cube cube = new Cube(new Point3D(0), new Point3D(70), new Plastic(new ColorRGB(1, 0, 0)));
+            HitInfo hitInfo = cube.Intersect(new Ray(new Point3D(0), new Vector3D(0)));
+            Assert.IsTrue(hitInfo.hasHit);
+
         }
+
+        [Test]
+        public void TestRayDirectionLessThanzero()
+        {
+            Cube cube = new Cube(new Point3D(20), new Point3D(70), new Plastic(new ColorRGB(1, 0, 0)));
+            HitInfo hitInfo = cube.Intersect(new Ray(new Point3D(0), new Vector3D(-2)));
+            Assert.IsFalse(hitInfo.hasHit);
+        }
+
+        [Test]
+        public void TestrayDirectionEqualsZeroAndCubeisHigher()
+        {
+            Cube cube = new Cube(new Point3D(50), new Point3D(70), new Plastic(new ColorRGB(1, 0, 0)));
+            HitInfo hitInfo = cube.Intersect(new Ray(new Point3D(0), new Vector3D(0)));
+            Assert.IsTrue(hitInfo.hasHit);
+        }
+
     }
 }

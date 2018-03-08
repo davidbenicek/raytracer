b2cae5b28428e946b5c66b42005472e7e758d086
Timothy <obimmatimothy@yahoo.com>
2018-03-08 13:42:16 +0000

    Added a test case

--- src/RayTracer.Tests/CamerasTesting/PerspectiveTest.cs
+++ src/RayTracer.Tests/CamerasTesting/PerspectiveTest.cs
@@ -17,12 +17,24 @@ namespace RayTracer.Tests.CamerasTesting
         Point3D w = new Point3D(0.016581, 0.0995037188, 0.9999999434);
 
         [Test]
-        public void TestFindRayDirection()
+        public void TestFindRayDirectionWithNormalize()
         {
             Vector3D rayDirection = point.x * u + point.y * v - distanceViewPlane * w;
             Vector3D newRayDirection= point.x * u + point.y * v - distanceViewPlane * w;
             rayDirection.Normalize();
             Assert.IsFalse(rayDirection.Equals(newRayDirection));
+
+        }
+
+        [Test]
+        public void TestFindRayDirectionWithoutNormalize()
+        {
+            Vector3D rayDirection =new Vector3D(0,0,0);
+            Vector3D newRayDirection = new Vector3D(rayDirection);
+            rayDirection.Normalize();
+            newRayDirection.Normalize();
+            Assert.IsTrue(rayDirection.Equals(newRayDirection));
+
         }
             
     }
